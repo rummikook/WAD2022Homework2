3cacd136813cc2f34c86754c677b6b0cad10d9b1
Juli Sokolov <julisokolov13@gmail.com>
2022-11-02 19:56:11 +0200

    dynamically creating posts

--- src/js/script.js
+++ src/js/script.js
@@ -14,17 +14,52 @@ function loadJSON(success){
 }
 
 function myData(Data) {
-    //all of data with metadata
-    console.log(Data)
-    //all of data without metadata
-    console.log(Data.record);
-}
+    var main = document.getElementsByTagName("main")
+    const posts = Data.record.posts
+    for (let i = 0; i < posts.length; i++) {
+        const post = posts[i]
 
-function myData(Data) {
-    //all of data with metadata
-    console.log(Data)
-        //all of data without metadata
-    console.log(Data.record);
+        var article = document.createElement("article");
+        var ul = document.createElement("ul");
+        ul.className="articleHeader"
+
+        var profile = document.createElement("li");
+        var profilePic = document.createElement("img");
+        profilePic.src="res/images/user.png"
+        profilePic.width=40
+        profilePic.height=40
+        profile.appendChild(profilePic)
+
+        var date = document.createElement("li");
+        const dateData = post.createTime
+        var dateText = document.createTextNode(dateData.substring(11,16) + " " + dateData.substring(0, 10))
+        date.appendChild(dateText)
+
+        ul.appendChild(profile)
+        ul.appendChild(date)
+        article.appendChild(ul)
+
+        var message = document.createElement("div")
+        var messageText = document.createTextNode(post.body)
+        message.appendChild(messageText)
+        article.appendChild(message)
+
+        if (post.picture!="") {
+            var postPicture = document.createElement("img");
+            postPicture.src=post.picture
+            postPicture.maxWidth="100%"
+            postPicture.maxHeight="100%"
+            article.appendChild(postPicture)
+        }
+
+        var likeButton = document.createElement("img");
+        likeButton.src="res/images/like.png"
+        likeButton.width=40
+        likeButton.height=40
+        article.appendChild(likeButton)
+
+        main[0].appendChild(article)
+    }
 }
 
 var a_parent = document.querySelectorAll(".picture");
