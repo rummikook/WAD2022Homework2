5433a13fa0e65c6a1772d128aa19ea8717c3b81c
Juli Sokolov <julisokolov13@gmail.com>
2022-11-02 20:07:00 +0200

    removed unused code block

--- src/js/script.js
+++ src/js/script.js
@@ -61,32 +61,3 @@ function myData(Data) {
         main[0].appendChild(article)
     }
 }
-
-var a_parent = document.querySelectorAll(".picture");
-var dd_menu_a = document.querySelectorAll(".dd_menu_item");
-
-a_parent.forEach(function(aitem) {
-
-    aitem.addEventListener("click", function() {
-        a_parent.forEach(function(aitem) {
-            aitem.classList.remove("active");
-        })
-        dd_menu_a.forEach(function(dd_menu_item) {
-            dd_menu_item.classList.remove("active");
-        })
-        aitem.classList.add("active");
-    })
-})
-
-dd_menu_a.forEach(function(dd_menu_item) {
-
-    dd_menu_item.addEventListener("click", function() {
-        dd_menu_a.forEach(function(dd_menu_item) {
-            dd_menu_item.classList.remove("active");
-        })
-        dd_menu_item.classList.add("active");
-    })
-})
-*/
-
-
