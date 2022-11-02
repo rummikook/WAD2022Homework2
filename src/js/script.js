127f090465f9f215f4edfc6922db557d5b392a82
Hanna Paalmäe <78278108+peroksiid@users.noreply.github.com>
2022-11-02 17:09:18 +0200

    dropdown menu script update unfinished

--- src/js/script.js
+++ src/js/script.js
@@ -19,3 +19,36 @@ function myData(Data) {
     //all of data without metadata
     console.log(Data.record);
 }
+
+function myData(Data) {
+    //all of data with metadata
+    console.log(Data)
+        //all of data without metadata
+    console.log(Data.record);
+}
+
+var a_parent = document.querySelectorAll(".picture");
+var dd_menu_a = document.querySelectorAll(".dd_menu_item");
+
+a_parent.forEach(function(aitem) {
+
+    aitem.addEventListener("click", function() {
+        a_parent.forEach(function(aitem) {
+            aitem.classList.remove("active");
+        })
+        dd_menu_a.forEach(function(dd_menu_item) {
+            dd_menu_item.classList.remove("active");
+        })
+        aitem.classList.add("active");
+    })
+})
+
+dd_menu_a.forEach(function(dd_menu_item) {
+
+    dd_menu_item.addEventListener("click", function() {
+        dd_menu_a.forEach(function(dd_menu_item) {
+            dd_menu_item.classList.remove("active");
+        })
+        dd_menu_item.classList.add("active");
+    })
+})
